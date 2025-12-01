e979f9e36772e19d5379f3b0cf7cd0ef9142f9ab
armkro <Armandas7080@gmail.com>
2025-12-01 23:29:50 +0200

    Created event endpoints

---
+++ repositories/event.repository.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/lib/prisma"
+
+export class EventRepository {
+  static async create(data: any) {
+    return await prisma.event.create({
+      data: data
+    });
+  }
+  static async findOne(id: number) {
+    return await prisma.event.findUnique({
+      where: { id: id }
+    });
+  }
+  static async findAll() {
+    return await prisma.event.findMany();
+  }
+  static async findAllByCommunity(communityId: number) {
+    return await prisma.event.findMany({
+        where: { communityId: communityId}
+    });
+  }
+  static async findAllByCreator(creatorId: number) {
+    return await prisma.event.findMany({
+        where: {creatorId: creatorId}
+    });
+  }
+  static async update(id: number, data: any) {
+    return await prisma.event.update({
+      where: { id },
+      data: data,
+    });
+  }
+  static async delete(id: number) {
+    return await prisma.event.delete({
+      where: { id }
+    });
+  }
+  static async isCreator(userId: number, eventId: number) {
+    const event = await prisma.event.findUnique({
+      where: { id: eventId }
+    });
+    return event?.creatorId === userId
+  }
+}
